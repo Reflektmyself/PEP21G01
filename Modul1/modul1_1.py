df4f4b881dc570a8ecfb6808adcf2eedf9c17edb
Reflektmyself <silviu.bugar@yahoo.com>
2021-02-17 21:35:38 +0200

    Prezentare 1

---
+++ Modul1/modul1_1.py
@@ -0,0 +1,26 @@
+print("Hello world")
+print('print\u200b("")')
+# printZWSP("")
+
+print(print)
+print(3)
+print("My name is:"," Bugar Silviu Valentin","1",sep="*", end="*")
+print("My name is:"," Bugar Silviu Valentin","1",sep="#", end="#")
+print("My name is:"," Bugar Silviu Valentin","1",sep="^", end="&")
+
+print()
+
+print_text1="Hello World"
+print_text2="Hello World"
+
+print(print_text1, print_text2)
+
+print_text1=123
+print_text2=456
+
+print(print_text1, print_text2)
+
+print("    ^")
+print("   ***")
+print("  *****")
+print("_________")
